dde777b746f6b2154443923038a7924edfe0d959
TimH04 <tim.haemmerli04@gmail.com>
2022-10-04 16:30:10 +0200

    Started with implementation of treasuremap

--- TreasureMap/app/src/main/java/ch/coop/treasuremap/MainActivity.java
+++ TreasureMap/app/src/main/java/ch/coop/treasuremap/MainActivity.java
@@ -1,27 +1,60 @@
 package ch.coop.treasuremap;
 
+import static org.osmdroid.tileprovider.util.StorageUtils.getStorage;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.preference.PreferenceManager;
 import android.view.View;
 
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
 
 import ch.coop.treasuremap.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.config.IConfigurationProvider;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private FusedLocationProviderClient fusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +76,84 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        IConfigurationProvider provider = Configuration.getInstance();
+        provider.setUserAgentValue(BuildConfig.APPLICATION_ID);
+        provider.setOsmdroidBasePath(getStorage());
+        provider.setOsmdroidTileCache(getStorage());
+        Context ctx = getApplicationContext();
+        Configuration.getInstance()
+                .load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        setContentView(R.layout.fragment_first);
+        MapView map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMaxZoomLevel(20.0);
+        map.setMultiTouchControls(true);
+        map.getZoomController()
+                .setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT);
+        IMapController controller = map.getController();
+        controller.setZoom(18.0);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        MyLocationNewOverlay myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this), map);
+        myLocationOverlay.enableMyLocation();
+        map.getOverlays().add(myLocationOverlay);
+
+        fusedLocationClient.getLocationAvailability();
+
+        Task<Location> locationTask = fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, new CancellationToken() {
+            @Override
+            public boolean isCancellationRequested() {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
+                return null;
+            }
+        });
+        LocationRequest locationRequestDistanceInterval = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setFastestInterval(0) // This MUST be set, otherwise no updates
+                .setSmallestDisplacement(1);
+
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setInterval(10000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationCallback mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+
+                if (locationResult != null && !locationResult.getLocations().isEmpty())
+                    controller.setCenter(new GeoPoint(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude()));
+            }
+
+            @Override
+            public void onLocationAvailability(LocationAvailability locationAvailability) {
+                              super.onLocationAvailability(locationAvailability);
+            }
+        };
+
+
+      /*  fusedLocationClient.requestLocationUpdates(locationRequest, mLocationCallback, new LocationListener()
+        {
+            @Override
+            public void onLocationChanged(Location location) {
+                // Handle your Distance based updates
+                controller.setCenter(new GeoPoint(location.getLatitude(), location.getLongitude()));
+
+            }
+        });
+        locationTask.addOnSuccessListener(location -> {
+            controller.setCenter(new GeoPoint(location.getLatitude(), location.getLongitude()));
+
+        });*/
+        /*.addOnSuccessListener(location -> {
+            controller.setCenter(new GeoPoint(location.getLatitude(), location.getLongitude()));
+        });*/
+
+
     }
 
     @Override
